b6c1a634ea6e995d4df09f45e58ad87bd1f9ec7c
Firmansyah Yanuar <fyfirman@gmail.com>
2020-07-16 13:24:08 +0700

    add pickerinput

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,15 +1,38 @@
 import React from 'react';
-import { TouchableOpacity, Text } from 'react-native';
-import { Actions } from 'react-native-router-flux';
+import { ScrollView } from 'react-native';
+import { Form, Item, Input, Label, Button, Text } from 'native-base';
+import { DatePicker, PickerInput } from '../../component';
+import styles from './Styles';
 
 const Register = () => {
-  const goToHome = () => {
-    Actions.home();
-  };
+  const genderData = [
+    { label: 'Laki-Laki', value: 'l' },
+    { label: 'Perempuan', value: 'p' }
+  ];
+
   return (
-    <TouchableOpacity style={{ margin: 128 }} onPress={goToHome}>
-      <Text>This is Register Page</Text>
-    </TouchableOpacity>
+    <ScrollView style={styles.container}>
+      <Form style={styles.loginForm}>
+        <Item inlineLabel>
+          <Label>Nama Lengkap</Label>
+          <Input />
+        </Item>
+        <Item>
+          <DatePicker label="Tanggal Lahir" />
+        </Item>
+        <Item inlineLabel>
+          <PickerInput label="Jenis Kelamin" data={genderData} />
+        </Item>
+        <Button full>
+          <Text>Sign In</Text>
+        </Button>
+      </Form>
+    </ScrollView>
   );
 };
+
+Register.navigationOptions = {
+  title: 'Informasi Pribadi'
+};
+
 export default Register;
