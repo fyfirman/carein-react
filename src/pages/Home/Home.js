d1fb558d6345ee2cd0a9f757beafff8f1cfd288e
Firmansyah Yanuar <fyfirman@gmail.com>
2020-07-23 16:12:21 +0700

    handle card button

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { View } from 'react-native';
-import { Container, Content } from 'native-base';
+import { Container } from 'native-base';
+import { Actions } from 'react-native-router-flux';
 import { Header, CardMenu } from './components';
 import styles from './styles';
 
 const Home = (props) => {
+  const goToSelectWorker = (workerType) => {
+    Actions.login(workerType);
+  };
+
   return (
     <Container>
       <Header />
@@ -12,15 +17,18 @@ const Home = (props) => {
         <CardMenu
           label="Dokter"
           imageSource={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}
+          onPress={() => goToSelectWorker('doctor')}
         />
         <CardMenu
           label="Perawat"
           imageSource={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}
+          onPress={() => goToSelectWorker('nurse')}
           reverse
         />
         <CardMenu
           label="Psikolog"
           imageSource={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}
+          onPress={() => goToSelectWorker('psychologist')}
         />
       </View>
     </Container>
--- src/pages/Home/components/CardMenu/CardMenu.js
+++ src/pages/Home/components/CardMenu/CardMenu.js
@@ -7,6 +7,7 @@ import styles from './styles';
 const propTypes = {
   label: PropTypes.string.isRequired,
   imageSource: PropTypes.objectOf(PropTypes.string).isRequired,
+  onPress: PropTypes.func.isRequired,
   reverse: PropTypes.bool
 };
 
@@ -15,11 +16,11 @@ const defaultProps = {
 };
 
 const StyledHeader = (props) => {
-  const { label, reverse, imageSource } = props;
+  const { label, reverse, imageSource, onPress } = props;
 
   return (
     <Card style={styles.root}>
-      <CardItem style={styles.content}>
+      <CardItem button style={styles.content} onPress={onPress}>
         {reverse && <Text style={styles.label}>{label}</Text>}
         <View style={styles.imageContainer}>
           <Image style={styles.image} source={imageSource} />
