123cb02fd19d1316c827dc72dd7c3e9109662709
Firmansyah Yanuar <fyfirman@gmail.com>
2020-07-24 15:27:17 +0700

    successfully get location

--- src/pages/SelectWorker/SelectWorker.js
+++ src/pages/SelectWorker/SelectWorker.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, Content } from 'native-base';
 import { Actions } from 'react-native-router-flux';
+import Geolocation from '@react-native-community/geolocation';
 import { CardWorker } from './components';
 import { Header } from '../../component';
 import styles from './styles';
@@ -56,6 +57,8 @@ const mockData = [
 
 const SelectWorker = (props) => {
   const { workerType } = props;
+
+  useEffect(() => Geolocation.getCurrentPosition((info) => console.log(info)));
 
   const backToHome = () => {
     Actions.pop();
