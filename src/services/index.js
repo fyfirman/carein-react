582fb54e6d651faac80b2c3bbd763ed6f81a1960
Firmansyah Yanuar <fyfirman@gmail.com>
2020-07-29 16:18:15 +0700

    add login services

--- src/services/index.js
+++ src/services/index.js
@@ -4,10 +4,12 @@ import Post from './Post';
 // Post
 const postRegister = (data) => Post('pasien', data);
 const postCheckRegister = (data) => Post('pasien?check', data);
+const postGenerateToken = (data) => Post('auth?remember=true', data);
 
 const API = {
   postRegister,
-  postCheckRegister
+  postCheckRegister,
+  postGenerateToken
 };
 
 export default API;
