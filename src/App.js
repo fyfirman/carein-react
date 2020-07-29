1ef7b540956a82ef4ef921630f36cd4a575a77b5
Firmansyah Yanuar <fyfirman@gmail.com>
2020-07-29 18:06:48 +0700

    create store and dispatch

--- src/App.js
+++ src/App.js
@@ -2,7 +2,26 @@ import React, { useEffect } from 'react';
 import { StatusBar, View } from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 import { Root } from 'native-base';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import Routes from './Routes';
+
+const initialState = {
+  token: null
+};
+
+const rootReducer = (state = initialState, action) => {
+  if (action.type === 'SET_TOKEN') {
+    return {
+      ...state,
+      token: `bearer ${state.token}`
+    };
+  }
+
+  return state;
+};
+
+const storeRedux = createStore(rootReducer);
 
 const App = () => {
   useEffect(() => {
@@ -10,12 +29,14 @@ const App = () => {
   }, []);
 
   return (
-    <View style={{ flex: 1 }}>
-      <StatusBar barStyle="light-content" backgroundColor="#2a83be" />
-      <Root>
-        <Routes />
-      </Root>
-    </View>
+    <Provider store={storeRedux}>
+      <View style={{ flex: 1 }}>
+        <StatusBar barStyle="light-content" backgroundColor="#2a83be" />
+        <Root>
+          <Routes />
+        </Root>
+      </View>
+    </Provider>
   );
 };
 export default App;
