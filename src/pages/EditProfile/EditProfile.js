f03e9f7545d8f2470f6c6284505f9b82bc809ccb
Firmansyah Yanuar <fyfirman@gmail.com>
2020-08-05 05:12:04 +0700

    handle edit profile data except date

--- src/pages/EditProfile/EditProfile.js
+++ src/pages/EditProfile/EditProfile.js
@@ -1,21 +1,38 @@
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { View } from 'react-native';
+import { connect } from 'react-redux';
 import { Toast, Container, Form, Button, Text, Content } from 'native-base';
 import { Actions } from 'react-native-router-flux';
 import validate from 'validate.js';
+import moment from 'moment';
 import API from '../../services';
 import { Header, DatePicker, PickerInput, TextInput } from '../../components';
 import styles from './styles';
 import { DateFormatter } from '../../helpers';
 import schema from './schema';
 
-const EditProfile = () => {
-  let isMounted = false;
+const propTypes = {
+  user: PropTypes.objectOf(PropTypes.any).isRequired
+};
+
+const defaultProps = {};
+
+const EditProfile = (props) => {
+  const { user } = props;
+
+  const bloodType = [
+    { label: 'AB', value: 'ab' },
+    { label: 'A', value: 'a' },
+    { label: 'B', value: 'b' },
+    { label: 'O', value: 'o' }
+  ];
 
   const [formState, setFormState] = useState({
     isValid: false,
     values: {
-      jk: 'l'
+      jk: 'l',
+      goldar: 'ab'
     },
     errors: {},
     touched: {},
@@ -23,6 +40,8 @@ const EditProfile = () => {
   });
 
   useEffect(() => {
+    let isMounted = true;
+
     const validateData = () => {
       const errors = validate(formState.values, schema);
 
@@ -33,10 +52,18 @@ const EditProfile = () => {
       }));
     };
 
-    isMounted = true;
     if (isMounted) {
       validateData();
+      setFormState({
+        ...formState,
+        values: user
+      });
     }
+
+    console.log(formState.values);
+    return () => {
+      isMounted = false;
+    };
   }, [formState.values]);
 
   const genderData = [
@@ -89,7 +116,7 @@ const EditProfile = () => {
             label="Nama Lengkap"
             onChangeText={(newValue) => handleChange('nama', newValue)}
             alertText={hasError('nama') ? formState.errors.nama[0] : null}
-            autoFocus
+            value={formState.values.nama}
           />
           <TextInput
             label="Tempat Lahir"
@@ -97,6 +124,7 @@ const EditProfile = () => {
             alertText={
               hasError('tempatLahir') ? formState.errors.tempatLahir[0] : null
             }
+            value={formState.values.tempatLahir}
           />
           <DatePicker
             label="Tanggal Lahir"
@@ -113,45 +141,44 @@ const EditProfile = () => {
             keyboardType="phone-pad"
             onChangeText={(newValue) => handleChange('noTelp', newValue)}
             alertText={hasError('noTelp') ? formState.errors.noTelp[0] : null}
+            value={formState.values.noTelp}
           />
           <TextInput
             label="Email"
             keyboardType="email-address"
             onChangeText={(newValue) => handleChange('email', newValue)}
             alertText={hasError('email') ? formState.errors.email[0] : null}
+            value={formState.values.email}
           />
           <TextInput
-            label="Username"
-            onChangeText={(newValue) => handleChange('username', newValue)}
+            label="Berat Badan"
+            onChangeText={(newValue) => handleChange('beratBadan', newValue)}
             alertText={
-              hasError('username') ? formState.errors.username[0] : null
+              hasError('beratBadan') ? formState.errors.beratBadan[0] : null
             }
+            keyboardType="numeric"
+            value={formState.values.beratBadan.toString()}
           />
           <TextInput
-            label="Password"
-            secureTextEntry
-            onChangeText={(newValue) => handleChange('password', newValue)}
+            label="Tinggi Badan"
+            onChangeText={(newValue) => handleChange('tinggiBadan', newValue)}
             alertText={
-              hasError('password') ? formState.errors.password[0] : null
+              hasError('tinggiBadan') ? formState.errors.tinggiBadan[0] : null
             }
+            keyboardType="numeric"
+            value={formState.values.tinggiBadan.toString()}
           />
-          <TextInput
-            label="Confirm Password"
-            secureTextEntry
-            onChangeText={(newValue) =>
-              handleChange('confirmPassword', newValue)
-            }
-            alertText={
-              hasError('confirmPassword')
-                ? formState.errors.confirmPassword[0]
-                : null
-            }
+          <PickerInput
+            label="Golongan Darah"
+            data={bloodType}
+            onValueChange={(newValue) => handleChange('goldar', newValue)}
+            selectedValue={formState.values.goldar}
           />
           <View>
             {formState.errorUserExist !== null ? renderErrorUserExist() : null}
           </View>
           <Button full onPress={handleSubmit} disabled={!formState.isValid}>
-            <Text>Next</Text>
+            <Text>Save</Text>
           </Button>
         </Form>
       </Content>
@@ -159,4 +186,13 @@ const EditProfile = () => {
   );
 };
 
-export default EditProfile;
+EditProfile.propTypes = propTypes;
+EditProfile.defaultProps = defaultProps;
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.userReducer.user
+  };
+};
+
+export default connect(mapStateToProps)(EditProfile);
--- src/pages/EditProfile/schema.js
+++ src/pages/EditProfile/schema.js
@@ -30,28 +30,5 @@ module.exports = {
     email: {
       message: 'tidak sesuai kriteria'
     }
-  },
-  username: {
-    presence: { allowEmpty: false },
-    format: {
-      pattern: Regex.username
-    },
-    length: {
-      minimum: 3,
-      maximum: 16
-    }
-  },
-  password: {
-    presence: { allowEmpty: false },
-    length: {
-      minimum: 8,
-      tooShort: 'minimal 8 karakter',
-      maximum: 128,
-      tooLong: 'maximal 128 karakter'
-    }
-  },
-  confirmPassword: {
-    equality: { attribute: 'password' },
-    presence: { allowEmpty: false }
   }
 };
