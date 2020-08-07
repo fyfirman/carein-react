06d94e7461c6d17c18133f80cee1f0ec8b1001b5
Firmansyah Yanuar <fyfirman@gmail.com>
2020-08-08 04:13:22 +0700

    fix routing home to profile

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BackHandler, View } from 'react-native';
+import { View } from 'react-native';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
@@ -12,15 +12,12 @@ import {
   Right,
   CardItem,
   Thumbnail,
-  Title,
-  Subtitle,
   Content
 } from 'native-base';
 import { Actions } from 'react-native-router-flux';
 import { Header, CardMenu, Feature } from './components';
 import styles from './styles';
 import Api from '../../services';
-import { BackAction } from '../../helpers';
 import { UserActions } from '../../redux/actions';
 
 const propTypes = {
@@ -32,8 +29,6 @@ const Home = (props) => {
   const { user, setUser } = props;
 
   useEffect(() => {
-    BackHandler.addEventListener('hardwareBackPress', BackAction.exit);
-
     const fetchUser = async () => {
       Api.getCheckAuth().then(
         (res) => {
@@ -54,85 +49,141 @@ const Home = (props) => {
     };
 
     fetchUser();
-
-    return () =>
-      BackHandler.removeEventListener('hardwareBackPress', backAction);
   }, []);
 
   return (
     <Container>
       <Content>
-      <Header name={user !== undefined ? user.nama : ''} />
-      <View style={styles.root}>
-      <View style={styles.subtitle}>
-        <Text style={{fontWeight:'bold',fontSize:14}}>Transaksi</Text>
-        <Text style={{fontSize:14}}>Lihat Semua</Text>
-      </View>
-      
-      <Card style={styles.card}>
-        <CardItem>
-          <Left>
-            <Thumbnail source={require('../../assets/me_here.jpeg')} style={styles.img}/>
-            <View style={styles.subcard}>
-            <Text style={{color:'black',fontWeight:'bold',fontSize:18}}>Marcell Antonius</Text>
-              <Text style={{color:'black',fontSize:12}}>Sedang dalam perjalanan</Text>
+        <Header name={user !== undefined ? user.nama : ''} />
+        <View style={styles.root}>
+          <View style={styles.subtitle}>
+            <Text style={{ fontWeight: 'bold', fontSize: 14 }}>Transaksi</Text>
+            <Text style={{ fontSize: 14 }}>Lihat Semua</Text>
+          </View>
+
+          <Card style={styles.card}>
+            <CardItem>
+              <Left>
+                <Thumbnail
+                  source={require('../../assets/me_here.jpeg')}
+                  style={styles.img}
+                />
+                <View style={styles.subcard}>
+                  <Text
+                    style={{ color: 'black', fontWeight: 'bold', fontSize: 18 }}
+                  >
+                    Marcell Antonius
+                  </Text>
+                  <Text style={{ color: 'black', fontSize: 12 }}>
+                    Sedang dalam perjalanan
+                  </Text>
+                </View>
+              </Left>
+              <Right>
+                <View style={styles.chat}>
+                  <Text style={styles.chatText}>Chat</Text>
+                </View>
+              </Right>
+            </CardItem>
+          </Card>
+
+          <Card style={styles.card}>
+            <CardItem>
+              <Left>
+                <Thumbnail
+                  source={require('../../assets/me_here.jpeg')}
+                  style={styles.img}
+                />
+                <View style={styles.subcard}>
+                  <Text
+                    style={{ color: 'black', fontWeight: 'bold', fontSize: 18 }}
+                  >
+                    Marcell Antonius
+                  </Text>
+                  <Text
+                    style={{
+                      color: 'black',
+                      fontSize: 14,
+                      paddingRight: '12%'
+                    }}
+                  >
+                    6 Agustus 2020
+                  </Text>
+                  <Text
+                    style={{ color: 'black', fontSize: 14, fontWeight: '600' }}
+                  >
+                    Rp. 100.000
+{' '}
+                    <Text
+                      style={{
+                        color: 'green',
+                        fontSize: 14,
+                        fontWeight: '600'
+                      }}
+                    >
+                      Selesai
+                    </Text>
+                  </Text>
+                </View>
+              </Left>
+            </CardItem>
+          </Card>
+
+          <Card style={styles.card}>
+            <CardItem>
+              <Text
+                style={{
+                  textAlign: 'center',
+                  marginVertical: 20,
+                  marginHorizontal: '12%',
+                  color: 'grey',
+                  fontSize: 18
+                }}
+              >
+                {' '}
+                Tidak ada transaksi tersedia
+{' '}
+              </Text>
+            </CardItem>
+          </Card>
+
+          <View style={styles.feature}>
+            <Text style={{ fontWeight: 'bold' }}>Pesan Tenaga Kesehatan</Text>
+            <View style={{ flexDirection: 'row' }}>
+              <Feature
+                title="Dokter"
+                imageSource={require('../../assets/dokter.png')}
+              />
+              <Feature
+                title="Psikolog"
+                imageSource={require('../../assets/psikolog.png')}
+              />
+              <Feature
+                title="Perawat"
+                imageSource={require('../../assets/perawat.png')}
+              />
             </View>
-          </Left>
-          <Right>
-            <View style={styles.chat}>
-              <Text style={styles.chatText} >Chat</Text>
-            </View>  
-          </Right>
-        </CardItem>
-      </Card>
+          </View>
 
-      <Card style={styles.card}>
-        <CardItem>
-          <Left>
-            <Thumbnail source={require('../../assets/me_here.jpeg')} style={styles.img}/>
-            <View style={styles.subcard}>
-              <Text style={{color:'black',fontWeight:'bold',fontSize:18}}>Marcell Antonius</Text>
-              <Text style={{color:'black',fontSize:14,paddingRight:'12%'}}>6 Agustus 2020</Text>
-              <Text style={{color:'black',fontSize:14,fontWeight:'600'}}>Rp. 100.000   <Text  style={{color:'green',fontSize:14,fontWeight:'600'}}>Selesai</Text></Text>
-            </View>
-          </Left>
-        </CardItem>
-      </Card>
-
-      <Card style={styles.card}>
-        <CardItem>
-          <Text style={{textAlign:'center',marginVertical:20,marginHorizontal:'12%',color:'grey',fontSize:18}}> Tidak ada transaksi tersedia </Text>
-        </CardItem>
-      </Card>
-
-      <View style={styles.feature}>
-        <Text style={{fontWeight:'bold'}}>Pesan Tenaga Kesehatan</Text>
-        <View style={{flexDirection:'row'}}>
-          <Feature title='Dokter' imageSource={require('../../assets/dokter.png')}/>
-          <Feature title='Psikolog' imageSource={require('../../assets/psikolog.png')}/>
-          <Feature title='Perawat' imageSource={require('../../assets/perawat.png')}/>
+          <View style={styles.cardContainer}>
+            <CardMenu
+              label="Dokter"
+              imageSource={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}
+              onPress={() => Actions.selectWorker({ workerType: 'dokter' })}
+            />
+            <CardMenu
+              label="Perawat"
+              imageSource={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}
+              onPress={() => Actions.selectWorker({ workerType: 'perawat' })}
+              reverse
+            />
+            <CardMenu
+              label="Psikolog"
+              imageSource={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}
+              onPress={() => Actions.selectWorker({ workerType: 'psikolog' })}
+            />
+          </View>
         </View>
-      </View>
-      
-      <View style={styles.cardContainer}>
-        <CardMenu
-          label="Dokter"
-          imageSource={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}
-          onPress={() => Actions.selectWorker({ workerType: 'dokter' })}
-        />
-        <CardMenu
-          label="Perawat"
-          imageSource={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}
-          onPress={() => Actions.selectWorker({ workerType: 'perawat' })}
-          reverse
-        />
-        <CardMenu
-          label="Psikolog"
-          imageSource={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}
-          onPress={() => Actions.selectWorker({ workerType: 'psikolog' })}
-        />
-      </View>
-      </View>
       </Content>
     </Container>
   );
