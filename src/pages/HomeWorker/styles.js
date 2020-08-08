cba0c86d78b0e791d4ccb4ac75ce5c4e01521414
marcell antonius <marcellantonius18@gmail.com>
2020-08-08 15:28:28 +0700

    'homeworker'

--- src/pages/HomeWorker/styles.js
+++ src/pages/HomeWorker/styles.js
@@ -4,7 +4,107 @@ module.exports = {
   content: {
     backgroundColor: 'green'
   },
-
+  heading:{
+    flexDirection: 'row',
+    marginHorizontal:16,
+    marginTop:30,
+    marginBottom:'7%', 
+    justifyContent: 'space-between'
+  },
+  thumbnail:{
+    borderRadius:20
+  },
+  infoMoney:{
+    flexDirection: 'row',
+    marginHorizontal:36,
+    marginTop:20,
+    marginBottom:'7%', 
+    justifyContent: 'space-between'
+  },
+  infoMoneyHeader:{
+    fontSize:12,
+    color:'rgba(6, 44, 60, 0.9)'
+  },
+  infoMoneyTotal:{
+    fontWeight:'bold',
+    fontSize:28,
+    color:'rgba(6, 44, 60, 0.9)'
+  },
+  map:{
+    width:396,
+    height:396,
+    backgroundColor:'grey',justifyContent:'center'
+  },
+  subHeadingLeft:{
+    fontWeight:'bold',
+    fontSize:14,
+    color:'rgba(6, 44, 60, 0.9)'
+  },
+  subHeadingRight:{
+    fontSize:14,
+    color:'rgba(6, 44, 60, 0.9)'
+  },
+  subCardOne:{
+    marginLeft:'-20%'
+  },
+  nameSubCardOne:{
+    color:'rgba(6, 44, 60, 0.9)',
+    fontWeight:'bold',
+    fontSize:18
+  },
+  statusSubCardOne:{
+    color:'rgba(6, 44, 60, 0.9)',
+    fontSize:12
+  },
+  btnSubCardOne:{
+    flexDirection:'row', 
+    marginTop:5
+  },
+  btnCancelDetailOne:{
+    backgroundColor:'#EB5757',
+    opacity:0.2,
+    width:85,
+    height:32,
+    borderRadius:8
+  },
+  btnCancelTextOne:{
+    fontSize:10,
+    color:'red'
+  },
+  btnSuccessDetailOne:{
+    backgroundColor:'#27AE60',
+    opacity:0.3,
+    width:75,
+    height:32,
+    marginLeft:4,
+    borderRadius:8
+  },
+  btnSuccessTextOne:{
+    fontSize:10,
+    color:'green'
+  },
+  btnSuccessDetailThree:{
+    width:72,
+    height:32,
+    marginLeft:4,
+    borderRadius:8
+  },
+  btnCancelDetailThree:{
+    backgroundColor:'#EB5757',
+    opacity:0.2,
+    width:72,
+    height:32,
+    borderRadius:8
+  },
+  btnCancelTextThree:{
+    fontSize:10,
+    color:'red',
+    textAlign:'center'
+  },
+  btnSuccessTextThree:{
+    fontSize:10,
+    color:'white'
+  },
   cardContainer: {
     marginTop:'5%',
     height: '75%',
@@ -18,10 +118,80 @@ module.exports = {
     marginBottom:'7%', 
     justifyContent: 'space-between',
   },
+  chatSubCardOne:{
+    backgroundColor:'#497CFB',
+    width:79,
+    height:32,
+    marginTop:45,
+    borderRadius:8
+  },
+  chatTextSubCardOne:{
+    fontSize:10,
+    color:'white',
+    textAlign:'center'
+  },
   card:{
     borderRadius:24,
     height:106,
   },
   subcard:{
+  },
+  cardBundle:{
+    marginVertical:14,
+    marginHorizontal:20
+  },
+  noInfoCard:{
+    borderRadius:24,
+    height:106,
+    backgroundColor:'#497CFB'
+  },
+  noInfoCardOFF:{
+    borderRadius:24,
+    height:106,
+    backgroundColor:'#497CFB',
+    opacity:0.6
+  },
+  noInfoCardBundle:{
+    marginVertical:30,
+    marginHorizontal:20
+  },
+  noInfoTextCard:{
+    fontSize:14,
+    fontWeight:'bold',
+    color:'white',
+    textAlign:'center'
+  },
+  switchCard:{
+    flex:1,
+    flexDirection:'row',
+    justifyContent:'center',
+  },
+  textCard:{
+    fontSize:14,
+    color:'white',paddingTop:3
+  },
+  option:{
+    flex:1,
+    flexDirection:'row',
+    justifyContent:'flex-end',
+    marginBottom:85
+  },
+  btnTrash:{
+    backgroundColor:'#EB5757',
+    opacity:0.2,
+    marginRight:9
+  },
+  iconTrash:{
+    color:'#EB5757'
+  },
+  btnAdd:{
+    backgroundColor:'#27AE60',
+    opacity:0.2,
+  },
+  iconAdd:{
+    color:'#27AE60'
+  },
+  cek:{
+    color:'red'
   }
 };
