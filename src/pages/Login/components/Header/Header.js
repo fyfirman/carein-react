3edf267e3c6b96a9e2a8908ab2d74dfa546a1834
Firmansyah Yanuar <fyfirman@gmail.com>
2020-08-09 05:24:32 +0700

    delete deadcode

--- src/pages/Login/components/Header/Header.js
+++ src/pages/Login/components/Header/Header.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import {View,Image, ImageBackground} from 'react-native';
+import { View, Image, ImageBackground } from 'react-native';
 import PropTypes from 'prop-types';
-import { Header,Button, Text,Left,Icon, Right, Title, Subtitle, Thumbnail } from 'native-base';
+import {
+  Header,
+  Button,
+  Text,
+  Left,
+  Icon,
+  Right,
+  Title,
+  Subtitle,
+  Thumbnail
+} from 'native-base';
 import styles from './styles';
 
 const propTypes = {
   name: PropTypes.string,
-  username: PropTypes.string,
+  username: PropTypes.string
 };
 
 const defaultProps = {
@@ -14,39 +24,34 @@ const defaultProps = {
 };
 
 const StyledHeader = (props) => {
-  const { name,username, ...rest } = props;
-  const handleSubmit = () => {
-    API.postGenerateToken(formState.values)
-      .then(
-        (res) => {
-          setToken(res.token);
-          LocalStorage.storeToken(res.token);
-          Toast.show({ text: res.message }, 1000);
-          setTimeout(() => Actions.home(), 1000);
-        },
-        (error) => {
-          Toast.show({ text: error.response.data.message }, 3000);
-        }
-      )
-      .catch((error) => {
-        Toast.show(
-          { text: `Something went wrong:  ${error.response.data.message}` },
-          3000
-        );
-      });
-  };
+  const { name, username, ...rest } = props;
+
   return (
-    <View style={{flex:1,flexDirection:'column',position:'relative'}} noShadow {...rest}> 
-      <ImageBackground style={styles.background} source={require('../../../../assets/WavyBackground.png')}/>
-        <View style={styles.textBundle}>
-          <Image style={{width:150,height:150,borderRadius:75}} large source={require('../../../../assets/marcell-white.jpg')} />
-          <Text style={styles.text}>Care.In</Text>
-        </View>
-        <View style={{position:'absolute',alignSelf:'flex-end'}}>
-          <Button transparent  onPress={() => Actions.loginWorker()}>
-            <Icon name='medkit-outline' style={{color:'white',fontSize:36}}/>
-          </Button>
-        </View>
+    <View
+      style={{ flex: 1, flexDirection: 'column', position: 'relative' }}
+      noShadow
+      {...rest}
+    >
+      <ImageBackground
+        style={styles.background}
+        source={require('../../../../assets/WavyBackground.png')}
+      />
+      <View style={styles.textBundle}>
+        <Image
+          style={{ width: 150, height: 150, borderRadius: 75 }}
+          large
+          source={require('../../../../assets/marcell-white.jpg')}
+        />
+        <Text style={styles.text}>Care.In</Text>
+      </View>
+      <View style={{ position: 'absolute', alignSelf: 'flex-end' }}>
+        <Button transparent onPress={() => Actions.loginWorker()}>
+          <Icon
+            name="medkit-outline"
+            style={{ color: 'white', fontSize: 36 }}
+          />
+        </Button>
+      </View>
     </View>
   );
 };
--- src/pages/LoginWorker/components/Header/Header.js
+++ src/pages/LoginWorker/components/Header/Header.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import {View,Image, ImageBackground} from 'react-native';
+import { View, Image, ImageBackground } from 'react-native';
 import PropTypes from 'prop-types';
-import { Header,Button, Text,Left,Icon, Right, Title, Subtitle, Thumbnail } from 'native-base';
+import {
+  Header,
+  Button,
+  Text,
+  Left,
+  Icon,
+  Right,
+  Title,
+  Subtitle,
+  Thumbnail
+} from 'native-base';
 import styles from './styles';
 
 const propTypes = {
   name: PropTypes.string,
-  username: PropTypes.string,
+  username: PropTypes.string
 };
 
 const defaultProps = {
@@ -14,39 +24,30 @@ const defaultProps = {
 };
 
 const StyledHeader = (props) => {
-  const { name,username, ...rest } = props;
-  const handleSubmit = () => {
-    API.postGenerateToken(formState.values)
-      .then(
-        (res) => {
-          setToken(res.token);
-          LocalStorage.storeToken(res.token);
-          Toast.show({ text: res.message }, 1000);
-          setTimeout(() => Actions.home(), 1000);
-        },
-        (error) => {
-          Toast.show({ text: error.response.data.message }, 3000);
-        }
-      )
-      .catch((error) => {
-        Toast.show(
-          { text: `Something went wrong:  ${error.response.data.message}` },
-          3000
-        );
-      });
-  };
+  const { name, username, ...rest } = props;
+
   return (
-    <View style={{flex:1,flexDirection:'column',position:'relative'}} noShadow {...rest}> 
-      <ImageBackground style={styles.background} source={require('../../../../assets/WavyBackground.png')}/>
-        <View style={styles.textBundle}>
-          <Icon name='medkit-outline' style={{color:'white',textAlign:'center',fontSize:130}}/>
-          <Text style={styles.text}>Masuk Sebagai Tenaga Kesehatan</Text>
-        </View>
-        <View style={{position:'absolute',alignSelf:'flex-end'}}>
-          {/* <Button transparent  onPress={() => Actions.loginWorker()}>
+    <View
+      style={{ flex: 1, flexDirection: 'column', position: 'relative' }}
+      noShadow
+      {...rest}
+    >
+      <ImageBackground
+        style={styles.background}
+        source={require('../../../../assets/WavyBackground.png')}
+      />
+      <View style={styles.textBundle}>
+        <Icon
+          name="medkit-outline"
+          style={{ color: 'white', textAlign: 'center', fontSize: 130 }}
+        />
+        <Text style={styles.text}>Masuk Sebagai Tenaga Kesehatan</Text>
+      </View>
+      <View style={{ position: 'absolute', alignSelf: 'flex-end' }}>
+        {/* <Button transparent  onPress={() => Actions.loginWorker()}>
             <Icon name='medkit-outline' style={{color:'white',fontSize:36}}/>
           </Button> */}
-        </View>
+      </View>
     </View>
   );
 };
