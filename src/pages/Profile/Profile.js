d6a4d97144708c3c1768c37945152f21223c5472
Firmansyah Yanuar <fyfirman@gmail.com>
2020-08-11 11:13:31 +0700

    fix riwayat kesehatan appear in worker

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, ActivityIndicator } from 'react-native';
 import PropTypes from 'prop-types';
 import { Container, List, Card, Content, Toast } from 'native-base';
 import { Actions } from 'react-native-router-flux';
@@ -25,9 +25,12 @@ const Profile = (props) => {
 
   const [userType, setUserType] = useState(UserType.PATIENT);
 
+  const [isLoaded, setIsLoaded] = useState(false);
+
   useEffect(() => {
     LocalStorage.getUserType().then((newUserType) => {
       setUserType(newUserType);
+      setIsLoaded(true);
     });
   }, []);
 
@@ -63,87 +66,91 @@ const Profile = (props) => {
           onEditPress={() => Actions.editProfile({ id: user.id })}
           onBackPress={() => Actions.pop()}
         />
-        <View style={styles.containter}>
-          <List style={{ marginTop: 10 }}>
-            <ProfileItem
-              title="Email"
-              item={user.email}
-              icon="mail-outline"
-              warna="blue"
-            />
-            <ProfileItem
-              title="Nomor Telepon"
-              item={user.noTelp}
-              icon="call-outline"
-              color="red"
-            />
+        {isLoaded ? (
+          <View style={styles.containter}>
+            <List style={{ marginTop: 10 }}>
+              <ProfileItem
+                title="Email"
+                item={user.email}
+                icon="mail-outline"
+                warna="blue"
+              />
+              <ProfileItem
+                title="Nomor Telepon"
+                item={user.noTelp}
+                icon="call-outline"
+                color="red"
+              />
+
+              {userType === UserType.PATIENT && (
+                <View>
+                  <ProfileItem
+                    title="Berat Badan"
+                    item={`${user.beratBadan} kg`}
+                    icon="man-outline"
+                    color="yellow"
+                  />
+                  <ProfileItem
+                    title="Tinggi Badan"
+                    item={`${user.tinggiBadan} cm`}
+                    icon="resize-outline"
+                    color="green"
+                  />
+                </View>
+              )}
+
+              {userType === UserType.WORKER && (
+                <View>
+                  <ProfileItem
+                    title="Profesi"
+                    item={`${StringBuilder.capitalizeLetter(user.jenis)}`}
+                    icon="medkit-outline"
+                    color="green"
+                  />
+                  <ProfileItem
+                    title="Harga"
+                    item={`Rp. ${user.harga}`}
+                    icon="cash-outline"
+                    color="green"
+                  />
+                </View>
+              )}
+            </List>
 
             {userType === UserType.PATIENT && (
               <View>
-                <ProfileItem
-                  title="Berat Badan"
-                  item={`${user.beratBadan} kg`}
-                  icon="man-outline"
-                  color="yellow"
-                />
-                <ProfileItem
-                  title="Tinggi Badan"
-                  item={`${user.tinggiBadan} cm`}
-                  icon="resize-outline"
-                  color="green"
-                />
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    marginHorizontal: 15,
+                    marginTop: 30,
+                    marginBottom: '3%',
+                    justifyContent: 'space-between'
+                  }}
+                >
+                  <Text style={{ fontWeight: 'bold', fontSize: 14 }}>
+                    Riwayat Kesehatan
+                  </Text>
+                  <TouchableOpacity onPress={() => Actions.medicalHistory()}>
+                    <Text style={{ fontSize: 14 }}>Lihat Semua</Text>
+                  </TouchableOpacity>
+                </View>
+
+                <Card style={styles.cardBundle}>
+                  {state.medicalHistory.map((item) => (
+                    <Riwayat
+                      index={item.id}
+                      penyakit={item.namaPenyakit}
+                      tanggal={item.tanggal}
+                    />
+                  ))}
+                </Card>
               </View>
             )}
-
-            {userType === UserType.WORKER && (
-              <View>
-                <ProfileItem
-                  title="Profesi"
-                  item={`${StringBuilder.capitalizeLetter(user.jenis)}`}
-                  icon="medkit-outline"
-                  color="green"
-                />
-                <ProfileItem
-                  title="Harga"
-                  item={`Rp. ${user.harga}`}
-                  icon="cash-outline"
-                  color="green"
-                />
-              </View>
-            )}
-          </List>
-
-          {userType === UserType.PATIENT && (
-            <View>
-              <View
-                style={{
-                  flexDirection: 'row',
-                  marginHorizontal: 15,
-                  marginTop: 30,
-                  marginBottom: '3%',
-                  justifyContent: 'space-between'
-                }}
-              >
-                <Text style={{ fontWeight: 'bold', fontSize: 14 }}>
-                  Riwayat Kesehatan
-                </Text>
-                <TouchableOpacity onPress={() => Actions.medicalHistory()}>
-                  <Text style={{ fontSize: 14 }}>Lihat Semua</Text>
-                </TouchableOpacity>
-              </View>
-
-              <Card style={styles.cardBundle}>
-                {state.medicalHistory.map((item) => (
-                  <Riwayat
-                    index={item.id}
-                    penyakit={item.namaPenyakit}
-                    tanggal={item.tanggal}
-                  />
-                ))}
-              </Card>
-            </View>
-          )}
-        </View>
+          </View>
+        ) : (
+          <ActivityIndicator />
+        )}
       </Content>
     </Container>
   );
