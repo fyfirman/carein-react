3987125302eeeca6c77d11be250c21c29a3d37ab
Firmansyah Yanuar <fyfirman@gmail.com>
2020-08-11 14:11:18 +0700

    fix transaction in home

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -6,3 +6,4 @@ export { default as StringBuilder } from './StringBuilder';
 export { default as LocationFormatter } from './LocationFormatter';
 export { default as BackAction } from './BackAction';
 export { default as Status } from './Status';
+export { default as Cost } from './Cost';
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,118 +1,124 @@
 import SPACING from '../../theme/Spacing';
 
 module.exports = {
-  root:{
-    marginHorizontal:16
+  root: {
+    marginHorizontal: 16
   },
-  textSubHeading:{
+  textSubHeading: {
     color: 'rgba(6, 44, 60, 0.9)',
     fontSize: 14,
-    fontWeight:'bold'
+    fontWeight: 'bold'
   },
-  textSubHeadingAll:{
-    fontSize: 14 ,color:'rgba(6, 44, 60, 0.9)'
+  textSubHeadingAll: {
+    fontSize: 14,
+    color: 'rgba(6, 44, 60, 0.9)'
   },
-  subtitle:{
+  subtitle: {
     flexDirection: 'row',
-    marginTop:22,
-    marginBottom:'7%', 
+    marginTop: 22,
+    marginBottom: '7%',
     justifyContent: 'space-between'
   },
-  
-  card:{
-    marginHorizontal:16,
-    overflow: 'hidden', 
-    paddingBottom:  5,
-    paddingRight:5,
-    borderRadius:24,
+
+  card: {
+    marginHorizontal: 16,
+    overflow: 'hidden',
+    paddingBottom: 5,
+    paddingRight: 5,
+    borderRadius: 24
   },
-  bundle:{
+  bundle: {
     backgroundColor: '#fff',
     shadowColor: '#000',
-    width:'auto',
-    height:106,
+    width: 'auto',
+    height: 106,
     shadowOffset: { width: 1, height: 1 },
-    shadowOpacity:  0.2,
+    shadowOpacity: 0.2,
     shadowRadius: 1,
     elevation: 5,
-    borderRadius:24
+    borderRadius: 24
   },
-  subcard:{
-    marginHorizontal:15,
-    alignSelf:'flex-start'
+  subcard: {
+    marginHorizontal: 15,
+    alignSelf: 'flex-start'
   },
-  subcardBundle:{
-    marginHorizontal:13
+  subcardBundle: {
+    marginHorizontal: 13
   },
-  textSubcard:{
-    color: 'rgba(6, 44, 60, 0.9)', 
-    fontWeight: 'bold', 
-    fontSize: 18 
+  textSubcard: {
+    color: 'rgba(6, 44, 60, 0.9)',
+    fontWeight: 'bold',
+    fontSize: 18
   },
-  subtextSubcard:{
-    color: 'rgba(6, 44, 60, 0.9)', 
-    fontSize: 12 
+  subtextSubcard: {
+    color: 'rgba(6, 44, 60, 0.9)',
+    fontSize: 12
   },
-  doneSubcard:{
+  doneSubcard: {
     color: 'rgba(6, 44, 60, 0.9)',
     fontSize: 14,
-    paddingLeft:'-5%'
+    paddingLeft: '-5%'
   },
-  doneInfoSubcard:{
-    color: 'green',
+  done: {
+    color: '#27AE60',
     fontSize: 14,
     fontWeight: '600'
   },
-  img:{
-    width:78,
-    height:78,
-    borderRadius:10
+  failed: {
+    color: '#EB5757',
+    fontSize: 14,
+    fontWeight: '600'
+  },
+  img: {
+    width: 78,
+    height: 78,
+    borderRadius: 10
   },
 
-  noTransSubcard:{
+  noTransSubcard: {
     textAlign: 'center',
     marginHorizontal: '12%',
     color: 'grey',
     fontSize: 18,
-    alignSelf:'center',
+    alignSelf: 'center'
   },
 
-  textBundle:{
-    flexDirection:'column',
-    flex:1,
-    justifyContent:'center'
+  textBundle: {
+    flexDirection: 'column',
+    flex: 1,
+    justifyContent: 'center'
   },
-  chat:{
-    backgroundColor:'#497CFB',
-    borderRadius:8,
-    height:35,
-    width:66, 
-    marginBottom:'-90%'
+  chat: {
+    backgroundColor: '#497CFB',
+    borderRadius: 8,
+    height: 35,
+    width: 66,
+    marginBottom: '-90%'
   },
-  chatBundle:{
-    alignSelf:'center',
-    marginRight:'-10%'
+  chatBundle: {
+    alignSelf: 'center',
+    marginRight: '-10%'
   },
-  chatText:{
-    fontSize:14,
-    paddingTop:'5%',
-    paddingBottom:'5%', 
-    color:'white',
-    textAlign:'center',
+  chatText: {
+    fontSize: 14,
+    paddingTop: '5%',
+    paddingBottom: '5%',
+    color: 'white',
+    textAlign: 'center'
   },
-  chatTextBundle:{
-    paddingLeft:'30%',
-    paddingRight:'30%'
+  chatTextBundle: {
+    paddingLeft: '30%',
+    paddingRight: '30%'
   },
-  feature:{
-    marginTop:30
+  feature: {
+    marginTop: 30
   },
   content: {
     backgroundColor: 'green'
   },
 
   cardContainer: {
-    marginTop:'5%',
+    marginTop: '5%',
     height: '75%',
     paddingLeft: SPACING.PADDING_HORIZONTAL,
     paddingRight: SPACING.PADDING_HORIZONTAL
