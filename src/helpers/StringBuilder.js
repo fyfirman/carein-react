51b65b1fed43a9a47852ac5db8446918b1bd09b3
Firmansyah Yanuar <fyfirman@gmail.com>
2020-08-11 10:52:55 +0700

    fix profile

--- src/helpers/StringBuilder.js
+++ src/helpers/StringBuilder.js
@@ -1,9 +1,12 @@
 import { BASE_URL } from 'react-native-dotenv';
 
 const addBaseURL = (string) => BASE_URL.slice(0, -7) + string;
+const capitalizeLetter = (string) =>
+  string[0].toUpperCase() + string.substring(1);
 
 const StringBuilder = {
-  addBaseURL
+  addBaseURL,
+  capitalizeLetter
 };
 
 export default StringBuilder;
