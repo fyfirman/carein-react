85ed9ce29556baf11c77c447749a0dbef0047541
Firmansyah Yanuar <fyfirman@gmail.com>
2020-08-11 11:06:43 +0700

    handle data on edit profile

--- src/components/HeaderFull/HeaderFull.js
+++ src/components/HeaderFull/HeaderFull.js
@@ -1,33 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Header, Text, Title, Button, Left, Right, Body, Icon } from 'native-base';
+import {
+  Header,
+  Text,
+  Title,
+  Button,
+  Left,
+  Right,
+  Body,
+  Icon
+} from 'native-base';
 import styles from './styles';
 
 const propTypes = {
   iconName: PropTypes.string.isRequired,
   iconNameRight: PropTypes.string.isRequired,
-  onPress: PropTypes.func.isRequired,
+  onLeftPress: PropTypes.func.isRequired,
+  onRightPress: PropTypes.func.isRequired,
   title: PropTypes.string.isRequired
 };
 
 const defaultProps = {};
 
 const StyledHeader = (props) => {
-  const { iconName,iconNameRight, onPress, title } = props;
+  const { iconName, iconNameRight, onLeftPress, onRightPress, title } = props;
 
   return (
     <Header style={styles.header}>
       <Left>
-        <Button transparent onPress={onPress}>
-          <Icon name={iconName} style={styles.icon}/>
+        <Button transparent onPress={onLeftPress}>
+          <Icon name={iconName} style={styles.icon} />
         </Button>
       </Left>
       <Body style={styles.body}>
         <Text style={styles.text}>{title}</Text>
       </Body>
       <Right>
-        <Button transparent onPress={onPress}>
-          <Icon name={iconNameRight} style={styles.iconRight}/>
+        <Button transparent onPress={onRightPress}>
+          <Icon name={iconNameRight} style={styles.iconRight} />
         </Button>
       </Right>
     </Header>
--- src/pages/EditProfile/EditProfile.js
+++ src/pages/EditProfile/EditProfile.js
@@ -8,10 +8,17 @@ import { Actions } from 'react-native-router-flux';
 import validate from 'validate.js';
 import Api from '../../services';
 import { UserActions } from '../../redux/actions';
-import { HeaderFull, DatePicker, PickerInput, TextInput } from '../../components';
+import {
+  HeaderFull,
+  DatePicker,
+  PickerInput,
+  TextInput
+} from '../../components';
+import { LocalStorage, DateFormatter } from '../../helpers';
 import styles from './styles';
-import { DateFormatter } from '../../helpers';
+
 import schema from './schema';
+import { UserType } from '../../constant';
 
 const propTypes = {
   user: PropTypes.objectOf(PropTypes.any).isRequired,
@@ -41,6 +48,17 @@ const EditProfile = (props) => {
     errorUserExist: null
   });
 
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  const [userType, setUserType] = useState(UserType.PATIENT);
+
+  useEffect(() => {
+    LocalStorage.getUserType().then((newUserType) => {
+      setUserType(newUserType);
+      setIsLoaded(true);
+    });
+  }, []);
+
   useEffect(() => {
     let isMounted = true;
 
@@ -62,7 +80,7 @@ const EditProfile = (props) => {
     return () => {
       isMounted = false;
     };
-  }, [formState.values]);
+  }, [formState.values, userType]);
 
   const genderData = [
     { label: 'Laki-Laki', value: 'l' },
@@ -123,96 +141,120 @@ const EditProfile = (props) => {
         iconNameRight="checkmark-outline"
         iconName="close-outline"
         title="Ubah Profil"
-        onPress={() => Actions.pop()}
+        onLeftPress={Actions.pop}
+        onRightPress={handleSubmit}
       />
       <Content style={styles.container}>
         <Form style={styles.loginFormWorker}>
-          {/* worker */}
-          <TextInput
-            label="Nama Lengkap"
-            onChangeText={(newValue) => handleChange('nama', newValue)}
-            alertText={hasError('nama') ? formState.errors.nama[0] : null}
-            value={formState.values.nama}
-          />
-          <TextInput
-            label="No telepon"
-            keyboardType="phone-pad"
-            onChangeText={(newValue) => handleChange('noTelp', newValue)}
-            alertText={hasError('noTelp') ? formState.errors.noTelp[0] : null}
-            value={formState.values.noTelp}
-          />
-          <TextInput
-            label="Email"
-            keyboardType="email-address"
-            onChangeText={(newValue) => handleChange('email', newValue)}
-            alertText={hasError('email') ? formState.errors.email[0] : null}
-            value={formState.values.email}
-          />
-          {/* <TextInput
-            label="Nama Lengkap"
-            onChangeText={(newValue) => handleChange('nama', newValue)}
-            alertText={hasError('nama') ? formState.errors.nama[0] : null}
-            value={formState.values.nama}
-          />
-          <TextInput
-            label="Tempat Lahir"
-            onChangeText={(newValue) => handleChange('tempatLahir', newValue)}
-            alertText={
-              hasError('tempatLahir') ? formState.errors.tempatLahir[0] : null
-            }
-            value={formState.values.tempatLahir}
-          />
-          <DatePicker
-            label="Tanggal Lahir"
-            onDateChange={(newValue) => handleChange('tglLahir', newValue)}
-            placeHolderText={DateFormatter.getLegibleDate(
-              formState.values.tglLahir
-            )}
-          />
-          <PickerInput
-            label="Jenis Kelamin"
-            data={genderData}
-            onValueChange={(newValue) => handleChange('jk', newValue)}
-            selectedValue={formState.values.jk}
-          />
-          <TextInput
-            label="No telepon"
-            keyboardType="phone-pad"
-            onChangeText={(newValue) => handleChange('noTelp', newValue)}
-            alertText={hasError('noTelp') ? formState.errors.noTelp[0] : null}
-            value={formState.values.noTelp}
-          />
-          <TextInput
-            label="Email"
-            keyboardType="email-address"
-            onChangeText={(newValue) => handleChange('email', newValue)}
-            alertText={hasError('email') ? formState.errors.email[0] : null}
-            value={formState.values.email}
-          />
-          <TextInput
-            label="Berat Badan"
-            onChangeText={(newValue) => handleChange('beratBadan', newValue)}
-            alertText={
-              hasError('beratBadan') ? formState.errors.beratBadan[0] : null
-            }
-            keyboardType="numeric"
-            value={formState.values.beratBadan.toString()}
-          />
-          <TextInput
-            label="Tinggi Badan"
-            onChangeText={(newValue) => handleChange('tinggiBadan', newValue)}
-            alertText={
-              hasError('tinggiBadan') ? formState.errors.tinggiBadan[0] : null
-            }
-            keyboardType="numeric"
-            value={formState.values.tinggiBadan.toString()}
-          />
-          <PickerInput
-            label="Golongan Darah"
-            data={bloodType}
-            onValueChange={(newValue) => handleChange('goldar', newValue)}
-            selectedValue={formState.values.goldar}
-          /> */}
+          {userType === UserType.WORKER && (
+            <View>
+              <TextInput
+                label="Nama Lengkap"
+                onChangeText={(newValue) => handleChange('nama', newValue)}
+                alertText={hasError('nama') ? formState.errors.nama[0] : null}
+                value={formState.values.nama}
+              />
+              <TextInput
+                label="No telepon"
+                keyboardType="phone-pad"
+                onChangeText={(newValue) => handleChange('noTelp', newValue)}
+                alertText={
+                  hasError('noTelp') ? formState.errors.noTelp[0] : null
+                }
+                value={formState.values.noTelp}
+              />
+              <TextInput
+                label="Email"
+                keyboardType="email-address"
+                onChangeText={(newValue) => handleChange('email', newValue)}
+                alertText={hasError('email') ? formState.errors.email[0] : null}
+                value={formState.values.email}
+              />
+            </View>
+          )}
+
+          {userType === UserType.PATIENT && isLoaded && (
+            <View>
+              <TextInput
+                label="Nama Lengkap"
+                onChangeText={(newValue) => handleChange('nama', newValue)}
+                alertText={hasError('nama') ? formState.errors.nama[0] : null}
+                value={formState.values.nama}
+              />
+              <TextInput
+                label="Tempat Lahir"
+                onChangeText={(newValue) =>
+                  handleChange('tempatLahir', newValue)
+                }
+                alertText={
+                  hasError('tempatLahir')
+                    ? formState.errors.tempatLahir[0]
+                    : null
+                }
+                value={formState.values.tempatLahir}
+              />
+              <DatePicker
+                label="Tanggal Lahir"
+                onDateChange={(newValue) => handleChange('tglLahir', newValue)}
+                placeHolderText={DateFormatter.getLegibleDate(
+                  formState.values.tglLahir
+                )}
+              />
+              <PickerInput
+                label="Jenis Kelamin"
+                data={genderData}
+                onValueChange={(newValue) => handleChange('jk', newValue)}
+                selectedValue={formState.values.jk}
+              />
+              <TextInput
+                label="No telepon"
+                keyboardType="phone-pad"
+                onChangeText={(newValue) => handleChange('noTelp', newValue)}
+                alertText={
+                  hasError('noTelp') ? formState.errors.noTelp[0] : null
+                }
+                value={formState.values.noTelp}
+              />
+              <TextInput
+                label="Email"
+                keyboardType="email-address"
+                onChangeText={(newValue) => handleChange('email', newValue)}
+                alertText={hasError('email') ? formState.errors.email[0] : null}
+                value={formState.values.email}
+              />
+              <TextInput
+                label="Berat Badan"
+                onChangeText={(newValue) =>
+                  handleChange('beratBadan', newValue)
+                }
+                alertText={
+                  hasError('beratBadan') ? formState.errors.beratBadan[0] : null
+                }
+                keyboardType="numeric"
+                value={formState.values.beratBadan.toString()}
+              />
+              <TextInput
+                label="Tinggi Badan"
+                onChangeText={(newValue) =>
+                  handleChange('tinggiBadan', newValue)
+                }
+                alertText={
+                  hasError('tinggiBadan')
+                    ? formState.errors.tinggiBadan[0]
+                    : null
+                }
+                keyboardType="numeric"
+                value={formState.values.tinggiBadan.toString()}
+              />
+              <PickerInput
+                label="Golongan Darah"
+                data={bloodType}
+                onValueChange={(newValue) => handleChange('goldar', newValue)}
+                selectedValue={formState.values.goldar}
+              />
+            </View>
+          )}
+
           <View>
             {formState.errorUserExist !== null ? renderErrorUserExist() : null}
           </View>
