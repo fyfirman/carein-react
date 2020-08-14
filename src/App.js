cd2ff0dd5bfda59f50c8716b790635f74b1ffa4a
Firmansyah Yanuar <fyfirman@gmail.com>
2020-08-14 21:29:50 +0700

    remove deadcode in app

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import { StatusBar, View } from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 import { Root } from 'native-base';
 import { Provider } from 'react-redux';
-import Firebase, { PushController } from './services/Firebase';
+import { PushController } from './services/Firebase';
 import Routes from './Routes';
 import { store } from './helpers';
 
 const App = () => {
   useEffect(() => {
-    // Firebase.init();
     SplashScreen.hide();
   }, []);
 
